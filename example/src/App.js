697dcc87c906f048f7560a067513ba4a4381f56f
shaho <toofani@gmail.com>
2021-05-27 11:33:50 +0430

    Works with upgraded default packages

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'odds-widget'
 import 'odds-widget/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Create React Library Example 2ß 😄' />
 }
 
 export default App
