d255628f782ef99a392d8924a259ad388c5c65fd
shaho <toofani@gmail.com>
2021-05-27 17:19:10 +0430

    Works just fine as pure

--- example/src/App.js
+++ example/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { ExampleComponent } from 'odds-widget'
 
 const App = () => {
-  return <ExampleComponent telxt='Create React Library Example 2 ß😄' />
+  return <ExampleComponent text='Create React Library Example 2 ß😄' />
 }
 
 export default App
